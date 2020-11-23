0f632191ca89bd42d0f93cb74fc22c3495d564bd
coffee-dan <coffee-dan@protonmail.com>
2020-11-23 17:27:46 -0600

    Work in progress, more config and gatsby pages

---
+++ src/pages/portfolio-item.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/header'
+import Layout from '../components/layout'
+
+export default function About() {
+	return (
+		<Layout>
+			<Header HeaderText="Portfolio Item Page" />
+			<Header HeaderText="Something impressive will go here" />
+			<p>Such wow. Very React.</p>
+		</Layout>
+	)
+}
