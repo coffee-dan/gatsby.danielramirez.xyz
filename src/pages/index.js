3b7ef29011e89ba7ddfe8ff3e9f4bd6341a66a1c
coffee-dan <coffee-dan@protonmail.com>
2020-11-25 22:49:45 -0600

    Basic site

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ export default function Home() {
 	return (
 		<>
 			<Header />
+
+			{/* Introduction section */}
 			<section className="intro" id="home">
 				<h1 className="section__title section__title--intro">
 					Hi, I am <strong>Daniel Ramirez</strong>
@@ -16,6 +18,102 @@ export default function Home() {
 				</p>
 				<img alt="" />
 			</section>
+
+			{/* My Services section */}
+			<section class="my-services" id="services">
+				<h2 class="section__title section__title--services">
+					What I do
+				</h2>
+				<div class="services">
+					<div class="service">
+						<h3>Design + Development</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis
+							nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat.
+						</p>
+					</div>
+
+					<div class="service">
+						<h3>E-Commerce</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis
+							nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat.
+						</p>
+					</div>
+
+					<div class="service">
+						<h3>WordPress</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis
+							nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat.
+						</p>
+					</div>
+				</div>
+
+				<a href="#work" class="btn">
+					My Work
+				</a>
+			</section>
+
+			{/* About Me */}
+			<section class="about-me" id="about">
+				<h2 class="section__title section__title--about">Who I am</h2>
+
+				<p class="section__subtitle section__subtitle--about">
+					Designer & developer based out of NYC
+				</p>
+
+				<div class="about-me__body">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat.
+					</p>
+				</div>
+
+				<img alt="" class="about-me__img" />
+			</section>
+
+			{/* My Work */}
+			<section class="my-work" id="work">
+				<h2 class="section__title section__title--work">My work</h2>
+				<p class="section__subtitle section__subtitle--work">
+					A selection of my range of work
+				</p>
+
+				<div class="portfolio">
+					<a href="#" class="portfolio__item">
+						<img alt="" class="portfolio__img" />
+					</a>
+
+					<a href="#" class="portfolio__item">
+						<img alt="" class="portfolio__img" />
+					</a>
+
+					<a href="#" class="portfolio__item">
+						<img alt="" class="portfolio__img" />
+					</a>
+				</div>
+			</section>
+
 			<Footer />
 		</>
 	)
--- src/pages/portfolio-item.js
+++ src/pages/portfolio-item.js
@@ -1,13 +1,38 @@
 import React from 'react'
 import Header from '../components/header'
-import Layout from '../components/layout'
+// import Layout from '../components/layout'
 
 export default function About() {
 	return (
-		<Layout>
-			<Header HeaderText="Portfolio Item Page" />
-			<Header HeaderText="Something impressive will go here" />
-			<p>Such wow. Very React.</p>
-		</Layout>
+		<>
+			<section class="intro">
+				<h1 class="section__title section__title--intro">
+					The title <strong>of my project</strong>
+				</h1>
+				<p class="section__subtitle section__subtitle--intro">
+					A short subtitle
+				</p>
+				<img alt="" class="intro__img" />
+			</section>
+
+			<div class="portfolio-item-individual">
+				<p>
+					Voluptatibus, soluta blanditiis! Incidunt ea unde itaque
+					illo molestiae eligendi sint culpa nobis voluptas sapiente
+					voluptate, magnam ipsum eius earum?
+				</p>
+				<img alt="" />
+				<p>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+					Saepe et amet tenetur! Fugit sequi optio corrupti fugiat
+					ducimus consequatur incidunt?
+				</p>
+				<p>
+					Voluptatibus, soluta blanditiis! Incidunt ea unde itaque
+					illo molestiae eligendi sint culpa nobis voluptas sapiente
+					voluptate, magnam ipsum eius earum?
+				</p>
+			</div>
+		</>
 	)
 }
